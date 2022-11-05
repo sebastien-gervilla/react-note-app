c713f04969620abbd15423f3717338eb779b3441
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-11-05 17:45:20 +0100

    Completed Notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Note from './Note';
+import AddNote from './AddNote';
+import useLocalStorage from '../hooks/useLocalStorage';
 
-const Notes = () => {
+const Notes = ({ searchedNote }) => {
 
-    const [notes, setNotes] = useState([]);
+    const [notes, setNotes] = useLocalStorage('notes', []);
 
-    const addNote = (newNote) => setNotes({...notes, newNote});
+    const addNote = newNote => setNotes([...notes, newNote]);
 
-    const displayNotes = () => notes.map(note =>
-        <Note title={note.title} />
-    )
+    const editNote = newNote => setNotes(notes.map(
+        note => note.id === newNote.id ? newNote : note
+    ));
+
+    const deleteNote = noteId => setNotes(notes.filter(note => note.id !== noteId));
+
+    const displayNotes = () => notes
+        .filter(note => note.title.includes(searchedNote) || note.text.includes(searchedNote))
+        .map(note =>
+            <Note 
+                key={note.id} 
+                noteInfos={note}
+                editNote={editNote}
+                deleteNote={deleteNote}
+            />
+        );
 
     return (
         <div className="notes">
-
+            <AddNote addNote={addNote} />
+            {displayNotes()}
         </div>
     );
 };
