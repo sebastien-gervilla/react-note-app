e6274ca6addfd77307b949d9246892141d59a683
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-11-05 17:45:34 +0100

    Completed AppNotes

--- src/pages/AppNotes.js
+++ src/pages/AppNotes.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import Header from '../components/Header';
+import Notes from '../components/Notes';
 
 const AppNotes = () => {
+
+    const [searchedNote, setSearchedNote] = useState('');
+    
+    const changeSearchedValue = value => setSearchedNote(value);
+
     return (
         <section id="app-notes">
             <div className="main-area">
                 <div className="main-content">
 
-                    
+                    <Header searchedValue={searchedNote} changeSearchedValue={changeSearchedValue} />
+                    <Notes searchedNote={searchedNote} />
 
                 </div>
             </div>
